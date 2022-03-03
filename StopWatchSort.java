6880a29b6e756ae6efaaf5fb5284b6f425894eb9
soloadventures <solomon.arrington@snths.org>
2022-03-03 21:28:15 +0000

    fix v1

--- StopWatchSort.java
+++ StopWatchSort.java
@@ -1,4 +1,35 @@
 
+class StopWatch {
+    private long startTime;
+    private long endTime;
+    public StopWatch()
+    {
+        startTime=System.currentTimeMillis();
+    }
+    
+        public long getEndTime() {
+            return endTime;
+        }
+        public long getStartTime()
+        {
+            return startTime;
+        }
+        public void start()
+        {
+            startTime=System.currentTimeMillis();
+        }
+        public void stop()
+        {
+            endTime=System.currentTimeMillis();
+        }
+        
+        public long getElapsedTime()
+        {
+            return endTime-startTime;
+        }
+    
+    
+    }
 public class StopWatchSort {
     public static void main(String[] args) {
         int size = 100000;
