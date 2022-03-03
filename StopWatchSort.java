198f253432afe412506445cabe99cf6aaf2fdbb6
soloadventures <solomon.arrington@snths.org>
2022-03-03 18:37:42 +0000

    fix

---
+++ StopWatchSort.java
@@ -0,0 +1,40 @@
+public class StopWatchSort {
+    public static void main(String[] args) {
+
+        //your code
+        int size = 100000;
+        double[] list = new double[size];
+		for(int i=0;i<list.length;i++)
+		{
+			list[i]= ((Math.random()*1000)+0);
+		}
+
+        StopWatch stopWatch = new StopWatch();
+        selectionSort(list);
+        stopWatch.stop();
+
+        System.out.println("The sort time is " + stopWatch.getElapsedTime());
+    }
+
+    /** The method for sorting the numbers */
+    public static void selectionSort(double[] list) {
+        for (int i = 0; i < list.length - 1; i++) {
+            // Find the minimum in the list[i..list.length-1]
+            double currentMin = list[i];
+            int currentMinIndex = i;
+
+            for (int j = i + 1; j < list.length; j++) {
+                if (currentMin > list[j]) {
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+
+            // Swap list[i] with list[currentMinIndex] if necessary;
+            if (currentMinIndex != i) {
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+            }
+        }
+    }
+}
