c653ae89f7eabc6e393ce0cef51d6fee3d7007b8
soloadventures <solomon.arrington@snths.org>
2022-03-03 21:21:55 +0000

    f

--- StopWatchSort.java
+++ StopWatchSort.java
@@ -1,12 +1,11 @@
+
 public class StopWatchSort {
     public static void main(String[] args) {
-
-        //your code
         int size = 100000;
         double[] list = new double[size];
 		for(int i=0;i<list.length;i++)
 		{
-			list[i]= ((Math.random()*1000)+0);
+			list[i]= ((Math.random()*size+1)+0);
 		}
 
         StopWatch stopWatch = new StopWatch();
